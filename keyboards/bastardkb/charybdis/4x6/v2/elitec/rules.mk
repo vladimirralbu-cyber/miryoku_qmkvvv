c9796c142a8f85aa90736d8f7a07ec4f3dbb50b8
vladimirralbu-cyber <vladimirralbu@gmail.com>
2026-04-19 18:15:50 +0300

    Update rules.mk

--- keyboards/bastardkb/charybdis/4x6/v2/elitec/rules.mk
+++ keyboards/bastardkb/charybdis/4x6/v2/elitec/rules.mk
@@ -6,15 +6,15 @@ MOUSEKEY_ENABLE = no        # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
-NKRO_ENABLE = no            # Enable N-Key Rollover
+NKRO_ENABLE = yes            # Enable N-Key Rollover
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 AUDIO_ENABLE = no           # Audio output
 
 AUDIO_SUPPORTED = no        # Audio is not supported.
-RGB_MATRIX_SUPPORTED = yes  # RGB matrix is supported and enabled by default.
-RGBLIGHT_SUPPORTED = yes    # RGB underglow is supported, but not enabled by default.
-RGB_MATRIX_ENABLE = yes     # Enable keyboard RGB matrix functionality
+RGB_MATRIX_SUPPORTED = no  # RGB matrix is supported and enabled by default.
+RGBLIGHT_SUPPORTED = no    # RGB underglow is supported, but not enabled by default.
+RGB_MATRIX_ENABLE = no     # Enable keyboard RGB matrix functionality
 RGB_MATRIX_DRIVER = WS2812
 
 # Charybdis is a split 4x6 keyboard with a maximum of 5 thumb keys (3 on the
