f5a86932f78e0ebb870ab758a29eb93a83b42830
vladimirralbu-cyber <vladimirralbu@gmail.com>
2026-04-19 20:29:32 +0300

    Update config.h

--- keyboards/bastardkb/charybdis/4x6/v2/splinky_2/config.h
+++ keyboards/bastardkb/charybdis/4x6/v2/splinky_2/config.h
@@ -24,22 +24,21 @@
     { GP27, GP28, GP15, GP6, GP7, GP8 }
 
 /* Handedness. */
+
+
 #define MASTER_RIGHT
 
 // To use the handedness pin, resistors need to be installed on the adapter PCB.
 // If so, uncomment the following code, and undefine MASTER_RIGHT above.
-// #define SPLIT_HAND_PIN GP13
+// #define SPLIT_HAND_PIN GP15
 // #define SPLIT_HAND_PIN_LOW_IS_LEFT  // High -> right, Low -> left.
-
-/* RGB settings. */
-#define RGB_DI_PIN GP0
 
 /* SPI & PMW3360 settings. */
 #define SPI_DRIVER SPID0
-#define SPI_SCK_PIN GP18
+#define SPI_SCK_PIN GP22
 #define SPI_MISO_PIN GP20
-#define SPI_MOSI_PIN GP19
-#define POINTING_DEVICE_CS_PIN GP14
+#define SPI_MOSI_PIN GP23
+#define POINTING_DEVICE_CS_PIN GP16
 
 /* Reset. */
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET
