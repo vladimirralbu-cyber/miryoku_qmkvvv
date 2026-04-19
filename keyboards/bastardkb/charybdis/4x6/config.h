54eed971596385dc4388fa0235c2a029beb7a24f
vladimirralbu-cyber <vladimirralbu@gmail.com>
2026-04-19 21:15:54 +0300

    Update config.h

--- keyboards/bastardkb/charybdis/4x6/config.h
+++ keyboards/bastardkb/charybdis/4x6/config.h
@@ -1,6 +1,5 @@
 /*
- * Copyright 2021 Quentin LEBASTARD <qlebastard@gmail.com>
- * Copyright 2021 Charly Delay <charly@codesink.dev> (@0xcharly)
+ * Copyright 2022 Charly Delay <charly@codesink.dev> (@0xcharly)
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -18,14 +17,31 @@
 
 #pragma once
 
-#define DIODE_DIRECTION ROW2COL
+/* Key matrix configuration. */
+#define MATRIX_ROW_PINS \
+    { GP29, GP26, GP5, GP4, GP9 }
+#define MATRIX_COL_PINS \
+    { GP27, GP28, GP21, GP6, GP7, GP8 }
 
-/* Trackball angle adjustment. */
-#define ROTATIONAL_TRANSFORM_ANGLE -25
+/* Handedness. */
+#define MASTER_RIGHT
+
+// To use the handedness pin, resistors need to be installed on the adapter PCB.
+// If so, uncomment the following code, and undefine MASTER_RIGHT above.
+// #define SPLIT_HAND_PIN GP15
+// #define SPLIT_HAND_PIN_LOW_IS_LEFT  // High -> right, Low -> left.
 
 /* RGB settings. */
-#ifdef RGB_MATRIX_ENABLE
-#    define RGB_MATRIX_LED_COUNT 58
-#    define RGB_MATRIX_SPLIT \
-        { 29, 29 }
-#endif
+#define RGB_DI_PIN GP0
+
+/* SPI & PMW3360 settings. */
+#define SPI_DRIVER SPID0
+#define SPI_SCK_PIN GP22
+#define SPI_MISO_PIN GP20
+#define SPI_MOSI_PIN GP23
+#define POINTING_DEVICE_CS_PIN GP16
+
+/* Reset. */
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_LED GP17
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 1000U
